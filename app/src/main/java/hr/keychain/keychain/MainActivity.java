67bb32acf377aadd733157c14c50eb2fd7166ac6
Goran Topličanec <gtoplica@fo.hr>
2016-11-15 14:05:21 +0100

    Menu
    Dodavanje Menu

--- app/src/main/java/hr/keychain/keychain/MainActivity.java
+++ app/src/main/java/hr/keychain/keychain/MainActivity.java
@@ -3,6 +3,8 @@ package hr.keychain.keychain;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -45,5 +47,13 @@ public class MainActivity extends AppCompatActivity {
                                          }
                                      }
         );
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.main_menu, menu);
+        return true;
     }
 }
