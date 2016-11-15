ea0a6efcd8230c75c2d3fa792e1c09127d6e4c6a
Goran Topličanec <gtoplica@fo.hr>
2016-11-15 15:25:45 +0100

    Menu v1.3.
    popravk izbornika v1.3.

--- app/src/main/java/hr/keychain/keychain/IzbornikActivity.java
+++ app/src/main/java/hr/keychain/keychain/IzbornikActivity.java
@@ -11,6 +11,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 public class IzbornikActivity extends AppCompatActivity {
 
@@ -47,5 +49,28 @@ public class IzbornikActivity extends AppCompatActivity {
         MenuInflater inflater = getMenuInflater();
         inflater.inflate(R.menu.main_menu, menu);
         return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId())
+        {
+            case R.id.All_Locks_id:
+                Toast.makeText(getApplicationContext(), "All locks optionselected", Toast.LENGTH_LONG).show();
+                return true;
+            case R.id.Lock_id:
+                Toast.makeText(getApplicationContext(), "Lock option selected", Toast.LENGTH_LONG).show();
+                return true;
+            case R.id.NewLock_id:
+                Toast.makeText(getApplicationContext(), "New lock option selected", Toast.LENGTH_LONG).show();
+                return true;
+            case R.id.Unlock_id:
+                Toast.makeText(getApplicationContext(), "Unlock option selected", Toast.LENGTH_SHORT).show();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+
+        }
+
     }
 }
