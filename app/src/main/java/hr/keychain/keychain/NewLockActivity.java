12f69f594ecfcbb8091b2cee649bf49a67743212
ibelac <ibelac@foi.hr>
2016-11-30 22:37:24 +0100

    Toolbar in New Lock Activity

--- app/src/main/java/hr/keychain/keychain/NewLockActivity.java
+++ app/src/main/java/hr/keychain/keychain/NewLockActivity.java
@@ -10,11 +10,17 @@ import android.support.v7.widget.Toolbar;
 
 public class NewLockActivity extends IzbornikActivity {
 
+    private Toolbar toolbar;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setTitle("NEW LOCK");
         setContentView(R.layout.activity_new_lock);
 
-
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
     }
 }
