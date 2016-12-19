06be35897dd0fb92c9f38428fd6c20b42f896dea
Sasa Posloncec <sasa.posloncec@gmail.com>
2016-12-20 00:04:20 +0100

    Login redirects to registration on create acc button click

--- app/src/main/java/hr/keychain/keychain/fragments/LoginFragment.java
+++ app/src/main/java/hr/keychain/keychain/fragments/LoginFragment.java
@@ -3,9 +3,12 @@ package hr.keychain.keychain.fragments;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import hr.keychain.keychain.R;
 
@@ -13,13 +16,46 @@ import hr.keychain.keychain.R;
  * Created by Saša Poslončec on 19.12.16..
  */
 
-public class LoginFragment extends Fragment {
+public class LoginFragment extends Fragment implements View.OnClickListener{
+    View view;
+    Button btnLogin;
+    Button btnCreateAcc;
 
     public LoginFragment () {}
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_login, container, false);
+        view = inflater.inflate(R.layout.fragment_login, container, false);
+        btnLogin = (Button) view.findViewById(R.id.btnLogin);
+        btnLogin.setOnClickListener(this);
+        btnCreateAcc = (Button) view.findViewById(R.id.btnCreateAcc);
+        btnCreateAcc.setOnClickListener(this);
+        return view;
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.btnLogin:
+                login();
+                break;
+
+            case R.id.btnCreateAcc:
+                createAccount();
+                break;
+        }
+    }
+
+    private void login(){
+
+    }
+
+    private void createAccount () {
+        FragmentManager fragmentManager = getFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        RegisterFragment registerFragment = new RegisterFragment();
+        fragmentTransaction.replace(R.id.login_registration_container, registerFragment);
+        fragmentTransaction.commit();
     }
 }
---
+++ app/src/main/java/hr/keychain/keychain/fragments/RegisterFragment.java
@@ -0,0 +1,23 @@
+package hr.keychain.keychain.fragments;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by Saša Poslončec on 19.12.16..
+ */
+
+public class RegisterFragment extends Fragment {
+
+    public RegisterFragment () {}
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return super.onCreateView(inflater, container, savedInstanceState);
+    }
+}
