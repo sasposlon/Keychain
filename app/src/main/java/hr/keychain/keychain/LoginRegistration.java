7a41de5e36cf33239f76e72c0c2115ebac6d6485
Sasa Posloncec <sasa.posloncec@gmail.com>
2016-12-19 22:23:35 +0100

    New activity for login and registration
    added new activity for login and registration with frame layout for login and registration fragments

---
+++ app/src/main/java/hr/keychain/keychain/LoginRegistration.java
@@ -0,0 +1,13 @@
+package hr.keychain.keychain;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class LoginRegistration extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login_registration);
+    }
+}
