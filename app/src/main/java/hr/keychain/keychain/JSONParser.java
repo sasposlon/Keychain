84b1e2a4bef613ce2cb75cdee5c929972040baef
ibelac <ibelac@foi.hr>
2017-01-05 19:39:58 +0100

    prilagoden PARSE

--- app/src/main/java/hr/keychain/keychain/JSONParser.java
+++ app/src/main/java/hr/keychain/keychain/JSONParser.java
@@ -10,17 +10,20 @@ import java.util.List;
 
 public class JSONParser {
 
-    public static List<String> parseKljuc(String content){
+    public static ArrayList<Key> parseKljuc(String content){
 
-    //Svi kljucevi samo nazivi
+    //Svi kljucevi (naziv, sirina, duzina)
     try{
         JSONArray jsonArray = new JSONArray(content);
-        List<String> kljucevi = new ArrayList<String>();
+        ArrayList<Key> kljucevi = new ArrayList<Key>();
 
         for (int i=0; i < jsonArray.length(); i++){
             JSONObject jsonobject = jsonArray.getJSONObject(i);
-            String naziv = jsonobject.getString("naziv");
-            kljucevi.add(naziv);
+            Key k = new Key();
+            k.setTitle(jsonobject.getString("naziv"));
+            k.setLatitude(jsonobject.getDouble("longitude"));
+            k.setLongitude(jsonobject.getDouble("latitude"));
+            kljucevi.add(k);
         }
         return kljucevi;
 
